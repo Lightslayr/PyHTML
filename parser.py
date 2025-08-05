a12d7e6d92efb7e794bd8f6a07979254eca9c472
= <=>
2025-08-05 17:02:52 +1000

    Initial: looping over in text

---
+++ parser.py
@@ -0,0 +1,10 @@
+def read_tag(ind, html_text):
+    print("todo")
+
+print("todo: parse")
+text = " <p1> hello </p1> "
+indices = [i for i, char in enumerate(text) if char == "<"]
+for i in indices:
+    read_tag()
+
+
