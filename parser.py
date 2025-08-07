3cb7ae113f7953b2c9a02f587f58cba88bae8ac4
= <=>
2025-08-07 17:17:06 +1000

    finished parser with node tree, attributes, regex and stored content, plus improvements to main

--- parser.py
+++ parser.py
@@ -1,3 +1,39 @@
+import re
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+@dataclass
+class Node:
+    tag: str
+    attrs: Optional[List[str]] = field(default_factory=list)
+    children: Optional[List[str]] = field(default_factory=list)
+    parent: Optional['Node'] = None
+
+elements_with_children = set(
+    'a', 'abbr', 'address', 'article', 'aside', 'audio', 'b', 'bdi', 'bdo', 'blockquote', 
+    'body', 'button', 'canvas', 'caption', 'cite', 'code', 'colgroup', 'data', 'datalist', 
+    'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'fieldset', 
+    'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 
+    'header', 'hgroup', 'html', 'i', 'iframe', 'ins', 'kbd', 'label', 'legend', 'li', 
+    'main', 'map', 'mark', 'menu', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 
+    'option', 'output', 'p', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 
+    'samp', 'script', 'section', 'select', 'small', 'span', 'strong', 'style', 'sub', 
+    'summary', 'sup', 'svg', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 
+    'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video'
+)
+def attributes_to_dict(html_tag):
+    attr_pattern = r'([a-zA-Z_-]+)\s*=\s*(?:"([^"]*)"|\'([^\']*)\'|([^\s>]+))|\b([a-zA-Z_-]+)\b'
+    matches = re.finditer(attr_pattern, html_tag)
+    
+    attrs_dict = {}
+    for match in matches:
+        if match.group(1):  
+            name = match.group(1)
+            value = match.group(2) or match.group(3) or match.group(4)
+            attrs_dict[name] = value
+        elif match.group(5):  
+            attrs_dict[match.group(5)] = True  
+    return attrs_dict
 def is_tag(pos, html_text):
     if pos + 1 >= len(html_text):  # Check if next char exists
         return False
@@ -31,7 +67,7 @@ def read_tag(pos, html_text):
             raise ValueError
             
     return tag_name, pos + 1
-    
+     
     
 
 
@@ -39,7 +75,7 @@ def read_tag(pos, html_text):
 
 def main():
     html_stack = []
-    text = " <html> <p1> hello </p1> </html> "
+    text = " <html> <img src=https.com/> <p1> hello </p1> </html> "
     indices = [i for i, char in enumerate(text) if char == "<"]
     for i in indices:
         tag, closing_char = read_tag(i, text)
